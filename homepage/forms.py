8eb08a4adc38252554bb726544f25ece0237bde8
Maghil Vannan M <mmaghil2k@gmail.com>
2021-03-25 19:50:36 +0530

    forms -> removed unnecessary commits, views.py-> added a custom validator for file, validators.py -> added custom validators.py

--- homepage/forms.py
+++ homepage/forms.py
@@ -4,7 +4,6 @@ class UploadForm(forms.Form):
    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Public name'}))
    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'About this File or Author'}))
    tags = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'#Tags ex:- #2020 #Nani #Lol'}))
-  ## filename = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control','accept':'.mp3','id':'file'}))
    filename = RestrictedFileField(accept="audio/wav",max_upload_size=2097152) 
 
 
--- homepage/validators.py
+++ homepage/validators.py
@@ -1,15 +1,43 @@
 from django.core.exceptions import ValidationError
 import fleep
+import json
+from ibm_watson import SpeechToTextV1
+from ibm_watson.websocket import RecognizeCallback, AudioSource
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import re
 
-def validateSoundAssets(value):
-    if not "wav" in value:
-        raise ValidationError("A valid school email must be entered in")
+def validateSoundAssets(value)->bool:
+    with open('temp.wav', 'wb+') as destination:
+        destination.write(value.read())
+    if (checkType() and checkAudio()):
+        return True
+    else :
+        return False
+
+
+def checkAudio():
+    authenticator = IAMAuthenticator('KTAC__c-sREYsNJlxDV82U5VQgGdWkgwwGOpWa3N5tdD')
+    service = SpeechToTextV1(authenticator=authenticator)
+    service.set_service_url("https://api.kr-seo.speech-to-text.watson.cloud.ibm.com/instances/8c376c7c-efef-4888-a376-f57fdae6b66a")    
+    with open("temp.wav",'rb') as audio_file:  
+        dic = json.loads(json.dumps(service.recognize(
+                                audio=audio_file,
+                                content_type='audio/wav',   
+                                timestamps=True,
+                                word_confidence=True,
+                                model='en-US_NarrowbandModel').get_result(), indent=2))
+    str = ""    
+    while bool(dic.get('results')):
+        str = dic.get('results').pop().get('alternatives').pop().get('transcript')+str[:]        
+    if(re.search("\*+",str)):
+        return False
     else:
-        return value
+        return True
 
-    # with open(value, "rb") as file:
-    #     info = fleep.get(file.read(128))
-
-    #     print(info.type)
-    #     print(info.extension) 
-    #     print(info.mime)
+def checkType():
+    with open("temp.wav", "rb") as file:
+        info = fleep.get(file.read(128))
+        if info.extension[0] == 'wav':
+            return True
+        else: 
+            return False
