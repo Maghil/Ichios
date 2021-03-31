9a0f844fa97c426e65a78744c91542e1ff58dd48
Girinath shah <girigirinath2172@gmail.com>
2021-03-31 18:46:00 +0530

    fixed validations for upload in javascript

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from homepage.fields import RestrictedFileField
 class UploadForm(forms.Form):
-   name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Public name'}))
-   description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'About this File or Author'}))
-   tags = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'#Tags ex:- #2020 #Nani #Lol'}))
+   name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Public name','id':'name'}))
+   description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'About this File or Author','id':'desc'}))
+   tags = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'#Tags ex:- #2020 #Nani #Lol','id':'tags'}))
    filename = RestrictedFileField(accept="audio/wav",max_upload_size=2097152) 
 
 
