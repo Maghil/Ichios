2112a69268151510b3132a2e945f1c5c5d5e963c
Girinath shah <girigirinath2172@gmail.com>
2021-04-01 11:04:50 +0530

    validated js and django for upload

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from homepage.fields import RestrictedFileField
 class UploadForm(forms.Form):
-   name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Public name','id':'name'}))
-   description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'About this File or Author','id':'desc'}))
-   tags = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'seperate tags by space','id':'tags'}))
+   name = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Public name','id':'name'}))
+   description = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'About this File or Author','id':'desc'}))
+   tags = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'seperate tags by space','id':'tags'}))
    filename = RestrictedFileField(accept="audio/wav",max_upload_size=2097152) 
 
 
